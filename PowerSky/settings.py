608228ee9bd93657c2f8727e038302770c6085a1
Shencheng <shencheng_1014@aliyun.com>
2019-08-27 11:56:19 +0800

    collectstatic 返回使用sqlite

--- PowerSky/settings.py
+++ PowerSky/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import pymysql
-
-pymysql.install_as_MySQLdb()
+import MySQLdb
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -85,14 +83,14 @@ WSGI_APPLICATION = 'PowerSky.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
-        'NAME': 'mydb',  # 数据库名，先前创建的
-        'USER': 'root',     # 用户名，可以自己创建用户
-        'PASSWORD': '18705735690',  # 密码
-        'HOST': 'localhost',  # mysql服务所在的主机ip 118.31.244.35
-        'PORT': '3306',         # mysql服务端口
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',   # 数据库引擎
+        # 'NAME': 'mydb',  # 数据库名，先前创建的
+        # 'USER': 'root',     # 用户名，可以自己创建用户
+        # 'PASSWORD': '18705735690',  # 密码
+        # 'HOST': 'localhost',  # mysql服务所在的主机ip 118.31.244.35
+        # 'PORT': '3306',         # mysql服务端口
     }
 }
 
